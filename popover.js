084dd68602d057681d4e5a02ddfa7b1c88675941
Zach Nichols <znichols1131@gmail.com>
2021-11-17 06:17:24 -0500

    initial commit

---
+++ popover.js
@@ -0,0 +1,35 @@
+$(function () {
+    $('.popover-Purdue').popover({
+      container: 'body',
+      trigger: 'hover',
+      placement: 'top',
+      content: 'I went to Purdue University.'
+    })
+  })
+
+  $(function () {
+    $('.popover-MNA').popover({
+      container: 'body',
+      trigger: 'hover',
+      placement: 'top',
+      content: 'I worked as a Mechanical Engineer for 3+ years.'
+    })
+  })
+
+  $(function () {
+    $('.popover-EFA').popover({
+      container: 'body',
+      trigger: 'hover',
+      placement: 'bottom',
+      content: 'I currently attend Eleven Fifty Academy.'
+    })
+  })
+
+  $(function () {
+    $('.popover-Indiana').popover({
+      container: 'body',
+      trigger: 'hover',
+      placement: 'bottom',
+      content: 'I currently live in Indianapolis, IN.'
+    })
+  })
